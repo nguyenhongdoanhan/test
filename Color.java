1ea8a57dbe1fa3258b4eeb4205dbd3669434c3ea
nguyenhongdoanhan <doanhan0405@gmail.com>
2024-11-09 15:25:13 +0700

    test

---
+++ Color.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Color {
+
+    public static void main(String[] args) {
+       ArrayList<String> colors = new ArrayList<>();
+       colors.add("Red");
+       colors.add("Yellow");
+       colors.add("Black");
+       colors.add("White");
+       for(int i=0;i<colors.size();i++){
+           System.out.println(colors.get(i));
+       }
+       for(String color:colors){
+           System.out.print(color);
+       }
+    }
+    
+}
---
+++ For2.java
@@ -0,0 +1,13 @@
+
+public class For2 {
+
+    public static void main(String[] args) {
+       System.out.println("T-minus");
+       for(int i=10;i>=1;i--){
+           System.out.println(i +", ");
+       }
+       System.out.println("blasttoff!");
+       System.out.println("The end.");
+    }
+    
+}
